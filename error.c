7801655b667bc7353b7d8cbb1a2cf6bda0464849
Sogoud2005 <sogoud2005@gmail.com>
2023-09-12 23:35:04 -0700

    simple shell

--- error.c
+++ error.c
@@ -26,4 +26,58 @@ int _erratoi(char *s)
 	}
 	return (res);
 }
+/**
+ * print_err - prints errors
+ * @info: return info struct
+ * @estr: error string
+ * Return: 0 or 1
+*/
+void print_err(info_t *info, char *estr)
+{
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
+}
+/**
+ * print_d - prints a decimal
+ * @input: input
+ * @fd: file desciptor to write to
+ * Return: number of characters printed
+*/
+int print_d(int input, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs, current;
+
+	if (fd == STDERR_FILENO)
+		_putchar = _eputchar;
+
+	if (input < 0)
+	{
+		_abs = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+	{
+		_abs = input;
+		current = _abs;
+	}
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
+	return (count);
+}
 
