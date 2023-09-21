dc44a3c656dfa5e3c2db4aad3eb240a94afc45b7
Sogoud2005 <sogoud2005@gmail.com>
2023-09-21 10:14:49 -0700

    simple shell

--- shell.h
+++ shell.h
@@ -31,3 +31,66 @@
 #define HIST_MAX 4096
 
 extern char **evniron;
+
+/**
+ * struct liststr - singly linked list
+ * @num: num field
+ * @str: string
+ * @next: pointer to the next node
+*/
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+/**
+ * struct passinfo - contains pseudo- arguments
+ * @arg: a string generated from getline containing arguments
+ * @argv: an array of strings generated from arg
+ * @path: a string path to the current command
+ * @argc: argv count
+ * @line_count: the error count
+ * @err_num: the error code for exit()
+ * @linecount_flag: l
+ * @fname: the program file_name
+ * @env: local copy of environ
+ * @environ: modified copy of environ from LL env
+ * @history: the history node
+ * @alias: the alias node
+ * @env_changed: on if environ changed
+ * @status: the return status of the last command
+ * @cmd_buf: address of pointer to cmd_buf on if chaining
+ * @cmd_buf_type: cmd_type &&, ||, ;
+ * @readfd: fd to read input line from
+ * @histcount: the history line number count
+*/
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+	char **cmd_buf;
+	int cmd_buf_type;
+	int readfd;
+	int histcount;
+} info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
+
+/**
+ *
